6c50d5c03d3cf4e73d6c62bc0fb5e1f6e2b00b98
hiro <hiro>
2022-05-27 15:12:01 +0900

    ツイート表示をbootstrapで調えた

--- resources/views/board/index.blade.php
+++ resources/views/board/index.blade.php
@@ -1,6 +1,9 @@
 @extends('board/layout')
 @section('content')
 @if (Auth::check())
+<header>
+  <h1>BBS</h1>
+</header>
 <p>USER: {{$user->name . ' (' . $user->email . ')'}}</p>
 @if(Storage::disk('local')->exists('public/profile/' . $user->id . '.jpg'))
         <td><figure>
@@ -15,6 +18,115 @@
             <input type="hidden" name="_token" value="{{ csrf_token() }}">
             <button type="submit" class="btn btn-default">ログアウト</button>
 </form>
+<div class="container">
+	<div class="row">
+    
+    <div class="col-sm-4 border justify-content-center">
+    <div class="text-center">1</div>
+    </div>
+    <div class="col-sm-4 border d-flex align-items-center justify-content-center" style="height:100px">
+      <form  style="margin: auto;">
+      <input class="form-control" type="text" name="address">
+      </form>
+    </div>
+    <div class="col-sm-4 border">
+      3
+    </div>
+  </div>
+  <div class="row justify-content-center">
+    bb
+  </div>
+  <div class="row">
+    cc
+  </div>
+</div>
+
+
+@foreach($boards as $board)
+<div class="container">
+	<div class="row">
+    <div class="col-sm-3">
+    </div>
+    <div class="col-sm-1 border border-end-0">
+      <div class="row">
+          @if(Storage::disk('local')->exists('public/profile/' . $board->user->id . '.jpg'))
+            <figure>
+              <img src="/storage/profile/{{$board->user-> id}}.jpg" width="50px" height="50px">
+              </figure>
+           @endif
+      </div>
+    </div>
+		<div class="col-sm-5 border border-start-0">
+			<div class="row">
+				<div class="col-sm-3">
+          <!--名前は左揃えにする-->
+          <div class="text-start font-weight-bold fs-4">
+            {{ $board->user->name }}
+          </div>
+        </div>
+				<div class="col-sm-5">
+				</div>
+				<div class="col-sm-4">
+          <!--テキストを右揃えするにはtext-endを指定する。-->
+          <div class="small text-secondary text-end">{{ $board->send_date }}</div>
+				</div>
+			</div>
+			<div class="row">
+        <a href="/board/{{ $board->id }}" style="text-decoration: none;color: #060606;">
+          <p>
+            {{ $board->post_text }}
+          </p>
+        </a>
+        @if(Storage::disk('local')->exists('public/images/' . $board->id . '.jpg'))
+          <figure>
+              <img src="/storage/images/{{$board->id}}.jpg" width="300px" height="300px">
+          </figure>
+        @endif
+			</div>
+      <div class="row">
+        <!--col内をセンタリングするにはd-flex以降全て必要-->
+				<div class="col-sm-3 d-flex align-items-center justify-content-center">
+          <a href="/board/{{ $board->id }}/reply" class="btn btn-light" >
+            <span class="small text-secondary">返信</span>
+          </a>
+				</div>
+				<div class="col-sm-3 d-flex align-items-center justify-content-center">
+          @if(!empty($board->reply->src_post_id))
+            <a href="/board/{{ $board->reply->src_post_id }}" class="btn btn-light">
+            <span class="small text-secondary">
+            <!--{{ $board->reply->src_post_id }}--> 
+            返信先
+            </span>
+          </a>
+          @endif
+				</div>
+				<div class="col-sm-3 d-flex align-items-center justify-content-center">
+          @if($board->user->id === Auth::id())
+            <a href="/board/{{ $board->id }}/edit" class="btn btn-light">
+              <span class="small text-secondary">編集</span>
+            </a>
+          @endif
+				</div>
+				<div class="col-sm-3 d-flex align-items-center justify-content-center">
+          @if($board->user->id === Auth::id())
+            <!--form内は下側に若干スペースが出来る。中央揃えにするにはmargin:autoが必要-->
+            <form action="/board/{{ $board->id }}" method="post" style="margin: auto;">
+              <input type="hidden" name="_method" value="DELETE">
+              <input type="hidden" name="_token" value="{{ csrf_token() }}">
+              <button type="submit" class="btn btn-danger">
+                <span class="small text-light">削除</span>
+              </button>
+            </form>
+          @endif
+        </div>
+			</div>
+		</div>
+    <div class="col-sm-3">
+    </div>
+  </div>
+</div>
+<br>
+@endforeach
 
 <div class="container ops-main">
 <div class="row">
