2fc8030558945511ead578e2ff37c1296a24148e
hiro <48009332+ehirokatsu@users.noreply.github.com>
2023-03-16 13:48:14 +0900

    テストコード追加

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -5,8 +5,15 @@ namespace Tests\Feature;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
+//factoryで使用
+use App\Models\User;
+
 class ExampleTest extends TestCase
 {
+    use RefreshDatabase;
+
+
+
     /**
      * A basic test example.
      *
@@ -14,8 +21,37 @@ class ExampleTest extends TestCase
      */
     public function test_the_application_returns_a_successful_response()
     {
-        $response = $this->get('/');
-
+        //ログイン画面に遷移可能なこと
+        $response = $this->get('/login');
         $response->assertStatus(200);
+
+        //登録画面に遷移可能なこと
+        $response = $this->get('/register');
+        $response->assertStatus(200);
+
+        //ホーム画面は認証が必要なこと
+        $response = $this->get('/');
+        $response->assertStatus(302);
+
+        //userを作成して認証できること
+        $user = User::factory()->create();
+        $response = $this->actingAs($user)->get('/');
+        $response->assertStatus(200);
+
+        $response = $this->actingAs($user)->get('/create');
+        $response->assertStatus(200);
+
+        $response = $this->actingAs($user)->get('/search');
+        $response->assertStatus(200);
+
+        
+
+        //存在しないページは404エラーとなること
+        $response = $this->get('/no_route');
+        $response->assertStatus(404);
+
+
+
+
     }
 }
