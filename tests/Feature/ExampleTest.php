9451e931c02e0e935c39cd21100b2d2278b14a11
hiro <48009332+ehirokatsu@users.noreply.github.com>
2023-03-23 14:30:27 +0900

    テストコード続き

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -23,32 +23,56 @@ class ExampleTest extends TestCase
      */
     public function test_the_application_returns_a_successful_response()
     {
-        //ログイン画面に遷移可能なこと
+        //ログイン画面に遷移可能なこと。対象のViewファイルを表示すること
         $response = $this->get('/login');
-        $response->assertStatus(200);
+        $response->assertStatus(200)->assertViewIs('auth.login');
 
-        //登録画面に遷移可能なこと
+        //登録画面に遷移可能なこと。対象のViewファイルを表示すること
         $response = $this->get('/register');
-        $response->assertStatus(200);
+        $response->assertStatus(200)->assertViewIs('auth.register');
 
-        //ホーム画面は認証が必要なこと
+        //ホーム画面は認証が必要なこと。対象のViewファイルを表示すること
         $response = $this->get('/');
         $response->assertStatus(302);
 
-        //userを作成して認証できること
+        //userを作成して認証できること。対象のViewファイルを表示すること
         $user = User::factory()->create();
         $response = $this->actingAs($user)->get('/');
-        $response->assertStatus(200);
+        $response->assertStatus(200)->assertViewIs('.index');
 
         $response = $this->actingAs($user)->get('/create');
-        $response->assertStatus(200);
+        $response->assertStatus(200)->assertViewIs('.create');
 
         $response = $this->actingAs($user)->get('/search');
-        $response->assertStatus(200);
+        $response->assertStatus(200)->assertViewIs('.search');
+
 
         //存在しないページは404エラーとなること
         $response = $this->get('/no_route');
         $response->assertStatus(404);
+
+        //ユーザー編集画面に遷移できること。対象のViewファイルを表示すること
+        $response = $this->actingAs($user)->get('/user/1/edit');
+        $response->assertStatus(200)->assertViewIs('.userEdit');
+
+        //投稿データを100個生成する
+        Board::factory()->count(100)->create();
+        $num = rand(1, 100);
+        
+        //作成したテストデータをランダムで選択し詳細画面に遷移できること。対象のViewファイルを表示すること
+        $response = $this->actingAs($user)->get('/'.$num);
+        $response->assertStatus(200)->assertViewIs('.show');
+
+        //作成したテストデータをランダムで選択し編集画面に遷移できること。対象のViewファイルを表示すること
+        $response = $this->actingAs($user)->get('/'.$num.'/edit');
+        $response->assertStatus(200)->assertViewIs('.edit');
+
+        //作成したテストデータをランダムで選択し返信画面に遷移できること。対象のViewファイルを表示すること
+        $response = $this->actingAs($user)->get('/'.$num.'/replyShow');
+        $response->assertStatus(200)->assertViewIs('.replyShow');
+        
+        //boardsテーブルにレコードが100件あること
+        $this->assertDatabaseCount('boards', 100);
 
         //テストデータを生成する
         User::factory()->create([
@@ -65,25 +89,5 @@ class ExampleTest extends TestCase
         ]);
 
 
-        //ユーザー編集画面に遷移できること
-        $response = $this->actingAs($user)->get('/user/1/edit');
-        $response->assertStatus(200);
-
-        //投稿データを100個生成する
-        Board::factory()->count(100)->create();
-        $num = rand(1, 100);
-        
-        //作成したテストデータをランダムで選択し詳細画面に遷移できること
-        $response = $this->actingAs($user)->get('/'.$num);
-        $response->assertStatus(200);
-
-        //作成したテストデータをランダムで選択し編集画面に遷移できること
-        $response = $this->actingAs($user)->get('/'.$num.'/edit');
-        $response->assertStatus(200);
-
-        //作成したテストデータをランダムで選択し返信画面に遷移できること
-        $response = $this->actingAs($user)->get('/'.$num.'/replyShow');
-        $response->assertStatus(200);
-        
     }
 }
